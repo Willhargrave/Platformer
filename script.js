6bdb523529b8128f44058a70206b5b08fcd8bd9e
will <willhargrave@hotmail.co.uk>
2023-02-20 20:31:12 +0900

    re-factoring

--- script.js
+++ script.js
@@ -11,47 +11,20 @@ const scaledCanvas = {
 
 const floorCollisions2D = []
 for (let i = 0; i < floorCollisions.length; i += 36) {
-    floorCollisions2D.push(floorCollisions.slice())
+    floorCollisions2D.push(floorCollisions.slice(i, i + 36))
 }
+
+floorCollisions2D.forEach((row) => {
+    row.forEach(symbol => {
+        if(symbol === 202) {
+
+        }
+    })
+})
 const gravity = 0.5
 
-class Sprite {
-    constructor({ position, imageSrc }) {
-        this.position = position
-        this.image = new Image()
-        this.image.src = imageSrc
-    }
-    draw() {
-        if (!this.image) return
-        c.drawImage(this.image, this.position.x, this.position.y)
-    }
-    update() {
-        this.draw()
-    }
-}
-class Player {
-    constructor(position) {
-        this.position = position
-        this.velocity = {
-            x: 0,
-            y: 1
-        }
-        this.height = 100
-    }
-    draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 100, this.height)
-    }
-    playerUpdate() {
-        this.draw()
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-        if (this.position.y + this.height + this.velocity.y < canvas.height)
-            this.velocity.y += gravity
-        else
-            this.velocity.y = 0
-    }
-}
+
+
 
 const player = new Player({
     x: 0,
@@ -122,3 +95,4 @@ window.addEventListener('keyup', (event) => {
             break
     }
 })
+
