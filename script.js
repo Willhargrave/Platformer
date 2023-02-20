78cc0c6ef155060b5b7dad9319d2034f4241b9a3
will <willhargrave@hotmail.co.uk>
2023-02-20 19:31:06 +0900

    background

--- script.js
+++ script.js
@@ -4,7 +4,26 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
+const scaledCanvas = {
+    width: canvas.width / 4,
+    height: canvas.height / 4
+}
 const gravity = 0.5
+
+class Sprite {
+    constructor({ position, imageSrc }) {
+        this.position = position
+        this.image = new Image()
+        this.image.src = imageSrc
+    }
+    draw() {
+        if (!this.image) return
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+    update() {
+        this.draw()
+    }
+}
 class Player {
     constructor(position) {
         this.position = position
@@ -18,7 +37,7 @@ class Player {
         c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, 100, this.height)
     }
-    update() {
+    playerUpdate() {
         this.draw()
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
@@ -46,15 +65,31 @@ const keys = {
         pressed: false
     }
 }
+
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0,
+    },
+    imageSrc: './images/background.png'
+})
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'white'
     c.fillRect(0, 0, canvas.width, canvas.height)
-    player.update()
-    player2.update()
+
+    c.save()
+    c.scale(4, 4)
+    c.translate(0, -background.image.height + scaledCanvas.height)
+    background.update()
+    c.restore()
+
+    player.playerUpdate()
+    player2.playerUpdate()
 
     player.velocity.x = 0
-    if(keys.d.pressed) player.velocity.x = 5
+    if (keys.d.pressed) player.velocity.x = 5
     else if (keys.a.pressed) player.velocity.x = -5
 }
 
@@ -64,22 +99,22 @@ window.addEventListener('keydown', (event) => {
     switch (event.key) {
         case 'd':
             keys.d.pressed = true
-        break
+            break
         case 'a':
             keys.a.pressed = true
-        break
+            break
         case 'w':
             player.velocity.y = -15
-        break
+            break
     }
 })
 window.addEventListener('keyup', (event) => {
     switch (event.key) {
         case 'd':
             keys.d.pressed = false
-        break
+            break
         case 'a':
             keys.a.pressed = false
-        break
+            break
     }
 })
